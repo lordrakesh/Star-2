72a85f1b0ad17259a8bd12c151e5c53e3d579257
Loli Sama <87615759+SimpFreakin@users.noreply.github.com>
2021-08-18 12:59:41 +0200

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -31,7 +31,7 @@ export default class Command extends BaseCommand {
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `👾 *Koneko Command List* 👾\n\n`
+            let text = `👾 *Konnichiwa (❤w❤). I am Koneko🌙* 👾\n\n`
             const keys = Object.keys(categories)
             for (const key of keys)
                 text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
