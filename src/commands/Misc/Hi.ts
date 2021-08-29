d20cb09e3f612386020470e7bb994e5f3ae8c871
Nakshatra <87998363+Nakshatra4762@users.noreply.github.com>
2021-08-29 19:55:32 +0530

    Update Hi.ts

--- src/commands/Misc/Hi.ts
+++ src/commands/Misc/Hi.ts
@@ -1,7 +1,5 @@
-import { MessageType } from '@adiwajshing/baileys'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
-import request from '../../lib/request'
 import WAClient from '../../lib/WAClient'
 import { ISimplifiedMessage } from '../../typings'
 
@@ -16,25 +14,6 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        
-        
-        
-return void M.reply(await request.buffer('https://wallpapercave.com/wp/wp5532688.png'),
-MessageType.image,
-            undefined,
-            undefined,
-            `*Konnichiwa~Desu*`
-
-                    
-)
-
-
+        return void (await M.reply(`Helloo ${M.sender.username}!`))
     }
-
-
-
-
-
-
-
-}                    
+}
